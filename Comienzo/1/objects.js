2d2ccdabe9ae0dad734a72c8f0c9056edee61fb3
SergioRaya17 <sergiorayarobles@gmail.com>
2024-05-01 09:09:04 +0200

    objects

---
+++ Comienzo/1/objects.js
@@ -0,0 +1,11 @@
+let obj1 = {};
+let obj2 = new Object();
+
+obj1.nombre = "Sergio";
+
+let persona = {
+    nombre: "Sergio",
+    apellidos: "Raya Robles"
+}
+
+console.log(persona.nombre + " " + persona.apellidos)
